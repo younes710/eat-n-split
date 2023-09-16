fa24edd5e23ee1b4e89b2d2c9985b7bf0546914e
Younes Abdi <realyoonesabdi@gmail.com>
2023-09-16 17:50:42 +0330

    Add friend component and move initialFriend to it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-const initialFriends = [
-    {
-        id: 118836,
-        name: 'Clark',
-        image: 'https://i.pravatar.cc/48?u=118836',
-        balance: -7,
-    },
-    {
-        id: 933372,
-        name: 'Sarah',
-        image: 'https://i.pravatar.cc/48?u=933372',
-        balance: 20,
-    },
-    {
-        id: 499476,
-        name: 'Anthony',
-        image: 'https://i.pravatar.cc/48?u=499476',
-        balance: 0,
-    },
-];
+import FriendsList from './components/friendsList/FriendsList';
 
 function App() {
-    return <></>;
+    return (
+        <div className='app'>
+            <div className='sidebar'>
+                <FriendsList />
+            </div>
+        </div>
+    );
 }
 
 export default App;
